4dc9443c77952f06c6e1c8d070a7fa050c044998
VincentC-D <124374685+VincentC-D@users.noreply.github.com>
2024-11-15 19:19:07 -0500

    Added home controller header

--- HPlusSport.Security.Web/Controllers/HomeController.cs
+++ HPlusSport.Security.Web/Controllers/HomeController.cs
@@ -4,6 +4,16 @@ using System.Diagnostics;
 
 namespace HPlusSport.Security.Web.Controllers;
 
+/*Course: 		Web Programming 3
+* Assessment: 	Milestone 3
+* Created by: 	Vincent Cloutier-Duval - 2235815
+* Date: 		<15> <November> 2024
+* Class Name: 	HomeController.cs
+* Description: 	This controller class manages the home page of this web application.
+*               It return the home view or an error view.
+* Time Task B):	2 hours. 
+*/
+
 public class HomeController : Controller
 {
     public IActionResult Index()
